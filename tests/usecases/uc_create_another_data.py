3e95abcfbd5a3cb10cd996e27f2d05e71f88cb5b
Ozair Jr <ozair.junior@luizalabs.com>
2025-05-27 16:26:57 -0300

    casos de usos

---
+++ tests/usecases/uc_create_another_data.py
@@ -0,0 +1,21 @@
+"""
+Cenários de testes para o caso de uso: Criação de dados para o.....
+
+
+"""
+
+import pytest
+
+"""
+Cenário 1:
+
+Criação do dado sendo que o seller está desativo
+Condição de entrada:
+Condição de saída
+"""
+def test_should_create_something_():
+    # Criação dos dados (Exemplo: Prepara o banco de dados
+    # com a 'massa de dados' necessária para o teste)
+    # Execução do teste (Exemplo: Chama uma ou mais apis)
+    # Validação dos dados (Valida bancos e/ou outros sistemas que são carregado
+    # com este ambiente/sistema)
